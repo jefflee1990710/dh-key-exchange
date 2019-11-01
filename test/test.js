0a63183efb4324ac7e535b13c3d0c6a6043a5715
Jeff Lee <jefflee1990710@gmail.com>
2019-11-02 00:35:09 +0800

    First commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+let getlargePrime = require('../index')
+
+describe('#get-large-prime', function() {
+    it('Geneate 100 1024bit prime numbers', async function() {
+        
+    });
+});
